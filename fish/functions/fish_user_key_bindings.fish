584b75e08e3f93b549904e1b0b8b2fb724ae3262
James Tan <jamestansx@gmail.com>
2024-02-19 22:52:21 +0800

    Update

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -6,4 +6,7 @@ function fish_user_key_bindings
 
 	# https://github.com/fish-shell/fish-shell/issues/7152#issuecomment-663575001
 	bind \cz 'fg 2>/dev/null; commandline -f repaint'
+
+	# https://stackoverflow.com/a/58382277
+	bind -M insert \cy accept-autosuggestion execute
 end
