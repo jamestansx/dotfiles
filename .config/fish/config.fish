419933f3115342ba33eeb5a6f39f495b72874ebc
James Tan <jamestansx@gmail.com>
2025-05-06 09:59:34 +0800

    fish: do not override QT env variable

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,11 +3,9 @@ if status is-login; and test -z "$WAYLAND_DISPLAY" -a "$XDG_VTNR" = 1
     set -gx XDG_CURRENT_DESKTOP river
     set -gx _JAVA_AWT_WM_NONREPARENTING 1
 
-    set -gx MOZ_ENABLE_WAYLAND 1
-    set -gx QT_QPA_PLATFORM wayland
-
     # Checkout https://github.com/swaywm/sway/wiki#i-cant-open-links-in-external-applications-in-firefox
     set -gx MOZ_DBUS_REMOTE 1
+    set -gx MOZ_ENABLE_WAYLAND 1
 
     set -l timestamp (date +%F-%R)
     exec river -log-level warning > /tmp/river-$timestamp.log 2>&1
--- .config/fish/functions/git.fish
+++ .config/fish/functions/git.fish
@@ -2,8 +2,6 @@ function git
     if test "$PWD" = "$HOME"
         set -fx GIT_DIR "$HOME"/.dotfiles
         set -fx GIT_WORK_TREE $HOME
-        command git $argv
-    else
-        command git $argv
     end
+    command git $argv
 end
