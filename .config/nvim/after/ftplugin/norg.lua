ebc931bb1f47d5135f60ea2cb9c8ecbd720907bb
James Tan <jamestansx@gmail.com>
2025-08-14 01:13:50 +0800

    nvim: remove neorg completely...

--- .config/nvim/after/ftplugin/norg.lua
+++
@@ -1,9 +0,0 @@
--- Start treesitter asynchronously
-vim.schedule(function()
-    if vim.treesitter.language.add("norg") then
-        vim.treesitter.start()
-    end
-end)
-
-vim.opt_local.conceallevel = 2
-vim.opt_local.concealcursor = "nvc"
