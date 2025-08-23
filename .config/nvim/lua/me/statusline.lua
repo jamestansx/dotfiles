3303fa172be9c271eabfd207be9febc43c2b761c
James Tan <jamestansx@gmail.com>
2025-08-23 12:18:19 +0800

    nvim: mirco optimize diagnostic display in statusline

--- .config/nvim/lua/me/statusline.lua
+++ .config/nvim/lua/me/statusline.lua
@@ -4,17 +4,17 @@ local state = {
     lsp_attached = false,
 }
 
--- TODO:-
+-- TODO:
 -- - check truncate condition on 'VimResized' event or within function itself
 -- - add customizable highlight color to components
 -- - add "busy" indicator
 local config = {
     lsp_icon = "●",
     diagnostic = {
-        { name = "ERROR", sign = "E", hl = "DiagnosticSignError" },
-        { name = "WARN", sign = "W", hl = "DiagnosticSignWarn" },
-        { name = "INFO", sign = "I", hl = "DiagnosticSignInfo" },
-        { name = "HINT", sign = "H", hl = "DiagnosticSignHint" },
+        [vim.diagnostic.severity.ERROR] = { sign = "E", hl = "DiagnosticSignError" },
+        [vim.diagnostic.severity.WARN] = { sign = "W", hl = "DiagnosticSignWarn" },
+        [vim.diagnostic.severity.INFO] = { sign = "I", hl = "DiagnosticSignInfo" },
+        [vim.diagnostic.severity.HINT] = { sign = "H", hl = "DiagnosticSignHint" },
     },
 }
 
@@ -38,25 +38,29 @@ end
 
 local diag_cb = function(args)
     local buf = args.buf
-    local current_buf = vim.api.nvim_get_current_buf()
     local count = vim.diagnostic.count(buf)
 
     if #count == 0 then
         state.diagcount[buf] = nil
     else
         local t = {}
-        for _, v in ipairs(config.diagnostic) do
-            local c = count[vim.diagnostic.severity[v.name]]
-            if c and c > 0 then
-                t[#t + 1] = ("%%#%s#%s%s%%*"):format(v.hl, v.sign, c)
+        for i, v in pairs(count) do
+            if v > 0 then
+                local s = config.diagnostic[i]
+                t[#t + 1] = ("%%#%s#%s:%s%%*"):format(s.hl, s.sign, v)
             end
         end
         state.diagcount[buf] = #t > 0 and table.concat(t, " ") or nil
     end
 
-    if current_buf == buf then
+    if vim.api.nvim_get_current_buf() == buf then
         vim.api.nvim__redraw({ buf = buf, statusline = true })
     end
+end
+
+local lsp_cb = function()
+    state.lsp_attached = vim.tbl_count(vim.lsp.get_clients()) > 0
+    vim.api.nvim__redraw({ statusline = true })
 end
 
 local join_component = function(comp) return comp == "" and "" or (" %s "):format(comp) end
@@ -73,13 +77,13 @@ end
 
 M.setup = function()
     local augroup = vim.api.nvim_create_augroup("me.statusline", { clear = true })
-    vim.api.nvim_create_autocmd("DiagnosticChanged", { group = augroup, callback = diag_cb })
+    vim.api.nvim_create_autocmd("DiagnosticChanged", {
+        group = augroup,
+        callback = vim.schedule_wrap(diag_cb),
+    })
     vim.api.nvim_create_autocmd({"LspAttach", "LspDetach"}, {
         group = augroup,
-        callback = vim.schedule_wrap(function()
-            state.lsp_attached = vim.tbl_count(vim.lsp.get_clients()) > 0
-            vim.api.nvim__redraw({ statusline = true })
-        end),
+        callback = vim.schedule_wrap(lsp_cb),
     })
 
     return "%{%v:lua.require'me.statusline'.statusline()%}"
