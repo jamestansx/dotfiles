c1399ca6cc7278a2fdb11ec1a85f8fe138b4eaf6
James Tan <jamestansx@gmail.com>
2026-02-24 17:15:33 +0800

    niri: finished experimenting with niri
    This concludes the testing with niri. Niri is here to stay!

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,22 +1,5 @@
 if status is-login; and test -z "$WAYLAND_DISPLAY" -a "$XDG_VTNR" = 1
-    # Checkout https://github.com/swaywm/sway/wiki#i-cant-open-links-in-external-applications-in-firefox
-    set -gx MOZ_DBUS_REMOTE 1
-    set -gx MOZ_ENABLE_WAYLAND 1
-
     exec niri-session -l
-end
-
-if status is-login; and test -z "$WAYLAND_DISPLAY" -a "$XDG_VTNR" = 2
-    set -gx XDG_SESSION_TYPE wayland
-    set -gx XDG_CURRENT_DESKTOP river
-    set -gx _JAVA_AWT_WM_NONREPARENTING 1
-
-    # Checkout https://github.com/swaywm/sway/wiki#i-cant-open-links-in-external-applications-in-firefox
-    set -gx MOZ_DBUS_REMOTE 1
-    set -gx MOZ_ENABLE_WAYLAND 1
-
-    set -l timestamp (date +%F-%R)
-    exec river -log-level warning > /tmp/river-$timestamp.log 2>&1
 end
 
 if status is-interactive
